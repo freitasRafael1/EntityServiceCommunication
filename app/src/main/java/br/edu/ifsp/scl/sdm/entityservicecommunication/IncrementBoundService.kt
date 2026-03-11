2147f793263457f720a825a5b029a191bc0afe3d
RafaelCostaFreitas <freitas.rafael1@aluno.ifsp.edu.br>
2026-03-11 06:29:15 -0300

    parte5

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementBoundService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementBoundService.kt
@@ -12,51 +12,21 @@ import android.util.Log
 
 class IncrementBoundService : Service() {
 
-    private inner class IncrementBoundServiceHandler(looper: Looper): Handler(looper) {
 
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-
-            msg.replyTo?.also {
-                clientMessenger = it
+    private val ibsBinder: IncrementBoundServiceInterface.Stub =
+        object : IncrementBoundServiceInterface.Stub(){
+            override fun increment(value: Int): Int = value + 1
             }
-
-
-                msg.data.getInt("VALUE", -1).also{
-                    if(it != -1){
-
-                    Log.v(this.javaClass.simpleName, "Returning increment value.")
-
-                    clientMessenger.send(Message.obtain().apply {
-                        data.putInt("VALUE", it + 1)
-                    })
-                }
-                    }
-
-                    }
-    }
-    private lateinit var  ibsMessenger: Messenger
-    private lateinit var ibsHandler: IncrementBoundServiceHandler
-    private lateinit var clientMessenger: Messenger
 
 
 
     override fun onBind(intent: Intent): IBinder {
         Log.v(this.javaClass.simpleName, "Entity bound to the service.")
-        ibsMessenger = Messenger(ibsHandler)
-        return ibsMessenger.binder
+        return ibsBinder
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
         Log.v(this.javaClass.simpleName, "Entity bound to the service.")
         return super.onUnbind(intent)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        HandlerThread(this.javaClass.simpleName).apply {
-            start()
-            ibsHandler = IncrementBoundServiceHandler(looper)
-        }
     }
 }
